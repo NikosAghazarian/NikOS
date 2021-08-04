d68d34e95fcefdbf05f3b65c0779bfd89f8dcb17
Nikos Aghazarian <nikosaghazarian@gmail.com>
2021-08-04 16:52:58 -0700

    new line

--- boot_sect.asm
+++ boot_sect.asm
@@ -4,19 +4,22 @@ mov ah, 0x0e  ;# int 10/ ah = 0eh -> scrolling teletype BIOS routine
 
 mov bp, 0x8000  ;# Stack base init
 mov sp, bp      ;# Stack ptr init
-push help_me
-pop bx
+
+mov edx, help_me
+call print_str
+jmp $
 
 print_str:
-    mov al, [bx]
-    int 0x10
-    add bx, 0x01  ;# next char
-    cmp al, 0x00  ;# if al != 0, continue
+    mov al, [edx]
+    int 0x10       ;# BIOS print interrupt
+    add edx, 0x01  ;# next char addr
+    mov al, [edx]  ;# read next char to check if continue
+    cmp al, 0      ;# if al != \0, continue
     jne print_str
-    ret
+    ret            ;# else, return
 
 help_me:
-    db 'AHHHHHHHHHHHHHHHHH', 0
+    db 'AHHHHHHHHHHHHHHHHH...', 0xd, 0xa, 0  ;# null terminated string, 0xd: carriage return, 0xa: newline
 
 times 510-($-$$) db 0
 
